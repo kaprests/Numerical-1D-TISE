9f9ca536446e3529b0b084e2c565b4988635dd37
Knut Andre Grytting Prestsveen <kaprests@gmail.com>
2019-03-05 09:30:12 +0100

    Adds most of the code required
    Plotting of well width doesn't work.

--- 1d_tusl.py
+++ 1d_tusl.py
@@ -2,15 +2,16 @@ import numpy as np
 from scipy.linalg import eigh_tridiagonal
 from matplotlib import pyplot as plt
 
+num_well = 4
 
 # Box potential
 V_box = lambda x: 0*x
 
 # n well potential
-def well(x, w, n_w, b,V0=-3000):
+def well(x, w, n_w, b, L,V0=-3000):
 	n = len(x)
-	V_mid_ex = np.array([[V0]*int(w*n) + [0]*int(b*n)]*n_w).flatten()
-	V_mid = V_mid_ex[:-int(b*n)]
+	V_mid_ex = np.array([[V0]*int(w*n/L) + [0]*int(b*n/L)]*n_w).flatten()
+	V_mid = V_mid_ex[:-int(b*n/L)]
 	
 	m = len(V_mid)
 	diff = n - m
@@ -25,11 +26,11 @@ def well(x, w, n_w, b,V0=-3000):
 	return V_vec
 
 
-def analyze(V, n_w):
+def analyze(n_w):
 	w = 0.1
-	b = 0.1
+	b = 0.05
 	L = (20 + n_w)*w + (n_w -1)*b
-	n = 2500
+	n = 1000
 	m = 1
 	h_bar = 1
 	delta_x = L/(n+1)
@@ -41,11 +42,12 @@ def analyze(V, n_w):
 	off_diag = np.zeros(n-1)
 		
 	if n_w == 0:
-		V_vals = V(x_vec)
+		V_vals = V_box(x_vec)
+		iter_lim = 5
 
 	else:
-		V_vals = V(x_vec, w, n_w, b)
-		print(V_vals)
+		V_vals = well(x_vec, w, n_w, b, L)
+		iter_lim = n_w*3
 
 	for i in range(n):
 		# Fills main diagonal
@@ -59,13 +61,36 @@ def analyze(V, n_w):
 
 	energies, wave_funcs = eigh_tridiagonal(main_diag, off_diag)
 	wave_funcs = wave_funcs.T
+	return energies, wave_funcs, iter_lim, L, V_vals
 
-	for i in range(5):
+def band_widths(lower, upper):
+	x_vec = np.linspace(lower, upper, upper-lower)
+	bw = np.zeros([3, upper-lower])
+	for i in range(lower, upper):
+		energies, _,_,_,_ = analyze(i)
+		# print("heyhey: ", energies, "\n")
+		for j in range(3):
+			bw[j][i-lower] = np.absolute(energies[i*j] - energies[(j+1)*i -1])
+			print("yoo: ", energies[(j+1)*i - 1])
+			#print(bw[j][i - lower])
+
+	for i in range(3):
+		plt.plot(x_vec, bw[i])
+	plt.show()
+	
+
+def plot_wave_funcs(energies, wave_funcs, iter_lim, L, V_vals):
+	x_vec = np.linspace(0, L, len(V_vals))
+	for i in range(iter_lim): 
 		plt.plot([0, L], [energies[i]]*2)
 		plt.plot(x_vec, V_vals)
-		plt.plot(x_vec, energies[i] + wave_funcs[i]*1000)
+		plt.plot(x_vec, energies[i] + wave_funcs[i]*3000)
+		plt.plot()
 	plt.show()
 
-analyze(V_box, 0)
-analyze(well, 5)
+E_b, w_b, iter_lim_b, L_b, V_vals_b = analyze(0)
+plot_wave_funcs(E_b, w_b, iter_lim_b, L_b, V_vals_b)
+energies, wave_funcs, iter_lim, L, V_vals = analyze(10)
+plot_wave_funcs(energies, wave_funcs, iter_lim, L, V_vals)
 
+band_widths(2, 50)
