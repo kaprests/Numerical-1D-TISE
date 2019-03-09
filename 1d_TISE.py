f49b48976d2d504b47f760cb3f6a0e9fb8f8af99
Knut Andre Grytting Prestsveen <kaprests@gmail.com>
2019-03-09 01:18:59 +0100

    Replaces 1d_tusl.py with 1d_TISE.py
    This script now does pretty much everything, and it does it right. All corresponding code in the
    notebook will be replaced with the contents of this script, and it should be regarded as our god
    and only source of truth.

---
+++ 1d_TISE.py
@@ -0,0 +1,117 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from scipy.optimize import fsolve
+from scipy.constants import hbar, m_e, eV
+from scipy.linalg import eigh_tridiagonal
+
+hbar *=1e9 #Plancks constant, scaled for nm as recomended by mr. MAN ;)
+L_well = 0.5 #nm - length/width of each potential well
+V0 = 10 #eV - depth of the wells
+n_well = 10 # number of datapoints per well
+n_bar = 5 # number of datapoints per barrer
+fact = (hbar**2)/(m_e*eV)
+
+
+def potential(num_w, V0=-V0):
+	V1 = np.array([[V0]*n_well + [0]*n_bar]).flatten() # one well with barrier to the right
+	V_mid = np.tile(V1, num_w)
+	V_front = np.zeros(n_well*10)
+	V_back = np.zeros(n_well*10 - n_bar)
+	V = np.append(V_front, V_mid)
+	V = np.append(V, V_back)
+	well_voids = max(num_w -1, 0)
+	L = num_w*L_well + well_voids*n_bar*(L_well/n_well) + 20*L_well
+	return V, L
+	
+
+def analyze(num_w, add_E_lvls = 0):
+	V, L = potential(num_w)
+	n = len(V)
+	delta_x = L/(n+1)
+	x_vec = np.linspace(0, L, n)
+	if num_w == 0:
+		num_E_lvls = 3 + add_E_lvls
+	else:
+		num_E_lvls = 3*num_w + add_E_lvls
+	
+	main_diag = np.ones(n) * fact/(delta_x**2) + V
+	off_diag = np.ones(n-1) * -fact/(2*delta_x**2)
+
+	energies, wave_funcs = eigh_tridiagonal(main_diag, off_diag)
+	wave_funcs = wave_funcs.T
+
+	return energies, wave_funcs, x_vec, num_E_lvls, L, V
+
+
+def plot_well_wave_funcs(num_w, add_E_lvls=0):
+	energies, wave_funcs, x_vec, iter_lim, L, V = analyze(num_w, add_E_lvls)
+	scale = V0
+	if num_w == 0:
+		scale = 1
+	for i in range(iter_lim):
+		plt.plot([0,L], [energies[i]]*2)
+		plt.plot(x_vec, V)
+		plt.plot(x_vec, energies[i] + wave_funcs[i]*scale)
+	plt.savefig("figW" + str(num_w) + "E" + str(iter_lim) + ".pdf")
+	plt.show()
+
+
+def band_widths(lower, upper):
+	x_vec = np.linspace(lower, upper, upper-lower)
+	band_widths = np.zeros([3, upper - lower])
+	for i in range(lower, upper):
+		energies,_,_,_,_,_ = analyze(i)
+		for j in range(3):
+			band_widths[j][i - lower] = np.absolute(energies[i*j] - energies[(j+1)*i -1])
+
+	for i in range(3):
+		plt.plot(x_vec, band_widths[i])
+	#plt.savefig("bw.pdf")
+	plt.show()
+
+
+def trancendent_sol():
+	a = L_well/2
+	
+	z0 = (a/hbar)*np.sqrt(2*m_e*V0*eV)
+	
+	RS = lambda z : np.sqrt((z0/z)**2 -1)
+	RS_asym = lambda z : -1/(np.sqrt((z0/z)**2 -1))
+	LS = lambda z : np.tan(z)
+
+	z_tan = np.linspace(0.1,4.5, 1000)
+	z = np.linspace(0.1,4.05, 1000)
+	for i in range(len(z_tan)):
+		if np.absolute(LS(z_tan[i])) > 9:
+			z_tan[i] = np.nan
+
+	f = lambda z : RS(z) - LS(z)
+	f_asym = lambda z : RS_asym(z) - LS(z)
+	
+	z_zeros = np.array([1.2, 3.5])
+	z_zeros = fsolve(f, z_zeros)
+
+	z_zeros_asym = 2
+	z_zeros_asym = fsolve(f_asym, z_zeros_asym)
+	
+	z_zeros = np.array([z_zeros[0], z_zeros_asym[0], z_zeros[1]])
+
+	print(z_zeros)
+
+	E = lambda z : ((z*hbar)**2)/(2*m_e*eV) - V0
+	for i in range(len(z_zeros)):
+		print("Energi #" + str(i+1) + ": ", E(z_zeros[i]))
+
+	plt.ylim(-5, 5)
+	plt.plot(z_tan, LS(z_tan))
+	plt.plot(z, RS(z))
+	plt.plot(z, RS_asym(z))
+	plt.show()
+
+
+plot_well_wave_funcs(0)
+plot_well_wave_funcs(1)
+plot_well_wave_funcs(10)
+plot_well_wave_funcs(50)
+trancendent_sol()
+band_widths(2, 50)
